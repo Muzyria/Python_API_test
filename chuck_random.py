eed6c91ca9b300b4f6432a0096ab709f02c8a938
Muzyria <fila090580@gmail.com>
2023-01-10 19:33:13 +0200

    new

--- chuck_random.py
+++ chuck_random.py
@@ -7,9 +7,36 @@ class Test_new_joke:
     def __init__(self):
         pass
 
-    def ntest_create_new_random_joke(self):
-        """Создание случайной шутки"""
-        url = "https://api.chucknorris.io/jokes/random"
+    # def ntest_create_new_random_joke(self):
+    #     """Создание случайной шутки"""
+    #     url = "https://api.chucknorris.io/jokes/random"
+    #     print(url)
+    #     result = requests.get(url)
+    #     print("Статус код : " + str(result.status_code))
+    #     assert 200 == result.status_code
+    #     if result.status_code == 200:
+    #         print("Успешно, Мы получили новую шутку")
+    #     else:
+    #         print("Повал. Запрос не верный")
+    #     result.encoding = 'utf-8'
+    #     print(result.text)
+    #     check = result.json()
+    #     # check_info = check.get("categories")
+    #     # print(check_info)
+    #     # assert check_info == []
+    #     # print("Категория верна")
+    #     check_info_value = check.get("value")
+    #     print(check_info_value)
+    #     name = "Chuck"
+    #     if name in check_info_value:
+    #         print("Chuck присутствует")
+    #     else:
+    #         print("Сhuck отсутствует")
+
+    def ntest_create_new_random_categories_joke(self):
+        """Создание случайной шутки на определенную тему"""
+        category = "sport"
+        url = f"https://api.chucknorris.io/jokes/random?category={category}"
         print(url)
         result = requests.get(url)
         print("Статус код : " + str(result.status_code))
@@ -25,14 +52,16 @@ class Test_new_joke:
         # print(check_info)
         # assert check_info == []
         # print("Категория верна")
-        check_info_value = check.get("value")
-        print(check_info_value)
-        name = "Chuck"
-        if name in check_info_value:
-            print("Chuck присутствует")
-        else:
-            print("Сhuck отсутствует")
+        # check_info_value = check.get("value")
+        # print(check_info_value)
+        # name = "Chuck"
+        # if name in check_info_value:
+        #     print("Chuck присутствует")
+        # else:
+        #     print("Сhuck отсутствует")
 
+# random_joke = Test_new_joke()
+# random_joke.ntest_create_new_random_joke()
 
-random_joke = Test_new_joke()
-random_joke.ntest_create_new_random_joke()
+sport_joke = Test_new_joke()
+sport_joke.ntest_create_new_random_categories_joke()
