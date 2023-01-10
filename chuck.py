0d2b3a62cc50ea0ab28a6eb12f5e2c4da2a13192
“Muzyria” <fila090580@gmail.com>
2023-01-10 16:54:59 +0200

    new

--- chuck.py
+++ chuck.py
@@ -5,5 +5,11 @@ print(url)
 result = requests.get(url)
 
 print("Статус код : " + str(result.status_code))
-assert 400 == result.status_code
+
+assert 200 == result.status_code
+if result.status_code == 200:
+    print("Успешноб Мы получили новую шутку")
+else:
+    print("Повал. Запрос не верный")
+result.encoding = 'utf-8'
 print(result.text)
