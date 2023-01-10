21a7977d1445ccd7446accb5fedef4bfb3fe8945
Muzyria <fila090580@gmail.com>
2023-01-10 20:24:28 +0200

    new

--- chuck_5_4_1_HW_.py
+++ chuck_5_4_1_HW_.py
@@ -5,27 +5,27 @@ class Test_new_joke:
     """Создание новой шутки"""
 
     def __init__(self):
-        pass
+        self.list_category = requests.get("https://api.chucknorris.io/jokes/categories").json()
 
     def ntest_create_new_random_categories_joke(self):
         """Создание случайной шутки на определенную тему"""
-        category = "sport"
-        url = f"https://api.chucknorris.io/jokes/random?category={category}"
-        print(url)
-        result = requests.get(url)
-        print("Статус код : " + str(result.status_code))
-        assert 200 == result.status_code
-        if result.status_code == 200:
-            print("Успешно, Мы получили новую шутку")
-        else:
-            print("Повал. Запрос не верный")
-        result.encoding = 'utf-8'
-        print(result.text)
-        check = result.json()
-        check_info = check.get("categories")
-        print(check_info)
-        assert check_info == ["sport"]
-        print("Категория верна")
+        for category in self.list_category:
+            url = f"https://api.chucknorris.io/jokes/random?category={category}"
+            print(url)
+            result = requests.get(url)
+            print("Статус код : " + str(result.status_code))
+            assert 200 == result.status_code
+            if result.status_code == 200:
+                print(f"Успешно, Мы получили новую шутку в категории {category}")
+            else:
+                print("Провал. Запрос не верный")
+            result.encoding = 'utf-8'
+            print(result.text)
+            check = result.json()
+            check_info = check.get("categories")
+            print(check_info)
+            assert check_info == [category]
+            print("Категория верна")
         # check_info_value = check.get("value")
         # print(check_info_value)
         # name = "Chuck"
@@ -34,8 +34,6 @@ class Test_new_joke:
         # else:
         #     print("Сhuck отсутствует")
 
-# random_joke = Test_new_joke()
-# random_joke.ntest_create_new_random_joke()
 
-sport_joke = Test_new_joke()
-sport_joke.ntest_create_new_random_categories_joke()
+run_category_joke = Test_new_joke()
+run_category_joke.ntest_create_new_random_categories_joke()
