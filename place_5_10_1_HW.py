1e4eaf9f3004a174ce04e0730a6431389471dd72
Muzyria <fila090580@gmail.com>
2023-01-12 23:23:25 +0200

    new

--- place_5_10_1_HW.py
+++ place_5_10_1_HW.py
@@ -8,38 +8,6 @@ class Test_new_location:
         self.base_url = "https://rahulshettyacademy.com"  # базовая url
         self.key = "?key=qaclick123"  # Параметр для всех запросов
 
-    def create_new_location(self):
-        """Создание новой локации метод POST"""
-
-        post_resource = "/maps/api/place/add/json"  # Ресурс метода пост
-        post_url = self.base_url + post_resource + self.key
-        print(post_url)
-        json_for_create_new_location = {
-                                        "location": {"lat": -38.383494, "lng": 33.427362},
-                                        "accuracy": 50,
-                                        "name": "Frontline house",
-                                        "phone_number": "(+91) 983 893 3937",
-                                        "address": "29, side layout, cohen 09",
-                                        "types": ["shoe park", "shop"],
-                                        "website": "http://google.com",
-                                        "language": "French-IN"
-                                         }
-
-        result_post = requests.post(post_url, json=json_for_create_new_location)
-        [print(k) for k in result_post.text.split(',')]
-
-        assert 200 == result_post.status_code
-        print("Успешно!!! Создана новая локация")
-        check_post = result_post.json()
-        check_info_post = check_post.get("status")
-        print(f'Статус код ответа : {check_info_post}')
-        assert check_info_post == "OK"
-        print("Статус код ответа верен")
-        place_id = check_post.get("place_id")
-        print(f'place_id ответа : {place_id}')
-        print()
-        return place_id
-
     def delete_new_location(self, place_id):
         """Удаление новой локации методом DELETE"""
         delete_resource = "/maps/api/place/delete/json"
@@ -50,7 +18,7 @@ class Test_new_location:
         print(result_delete.text)
         print(f'Статус код ответа : {result_delete.status_code}')
         assert 200 == result_delete.status_code
-        print("Успешно!!! Удаление новой локации прошло успешно")
+        print(f"Успешно!!! Удаление локации {place_id} прошло успешно")
         check_delete = result_delete.json()
         check_delete_info = check_delete.get("status")
         print(f'Сообщение : {check_delete_info}')
@@ -58,7 +26,7 @@ class Test_new_location:
         print("Сообщение верно")
 
     def check_new_location(self, num, place_id):
-        """Проверка создания новой локации метод GET"""
+        """Проверка локации метод GET"""
         print(f'Проверка №{num} для {place_id}')
         get_resource = "/maps/api/place/get/json"
         get_url = self.base_url + get_resource + self.key + "&place_id=" + place_id
@@ -66,21 +34,34 @@ class Test_new_location:
         result_get = requests.get(get_url)
         print(result_get.text)
         print(f'Статус код ответа : {result_get.status_code}')
-        assert 200 == result_get.status_code
-        print(f"Успешно!!! Проверка создания новой локации №{num} для {place_id}")
-        print()
+        try:
+            assert 200 == result_get.status_code
+            print(f"Успешно!!! Проверка локации №{num} для {place_id}")
+            print()
+            return True
+        except Exception:
+            print(f"Провал!!! Локация №{num} для {place_id} не найдена !!!")
+            print()
+            return False
 
-    def write_place_id_to_file(self):
-        """Создаем файл и записываем в него 5шт place_id"""
-        with open("list_place_id.txt", "w", encoding='utf-8') as file:
-            [file.write(self.create_new_location() + "\n") for _ in range(5)]
+
+    # def write_place_id_to_file(self):
+    #     """Создаем файл и записываем в него 5шт place_id"""
+    #     with open("list_place_id.txt", "w", encoding='utf-8') as file:
+    #         [file.write(self.create_new_location() + "\n") for _ in range(5)]
 
     def read_place_id_from_file(self):
         """Чтение place_id из файла и вызов метода GET"""
         with open("list_place_id.txt", "r", encoding='utf-8') as file:
             [self.check_new_location(num, line) for num, line in enumerate(file.read().split("\n"), 1) if line != '']
 
+    def read_place_id_and_delete(self):
+        """Чтение place_id из файла и вызов метода DELETE"""
+        with open("list_place_id.txt", "r", encoding='utf-8') as file:
+            [self.delete_new_location(line) for num, line in enumerate(file.read().split("\n"), 1)
+             if num in (2, 4) and line != '']
+
 
 new_place = Test_new_location()
-new_place.write_place_id_to_file()
-new_place.read_place_id_from_file()
+new_place.read_place_id_and_delete()  # Удаление 2 и 4 локации
+new_place.read_place_id_from_file()  # Проверка локаций и запись в новый файл
