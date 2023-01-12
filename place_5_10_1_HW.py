ff61e2cba838411722ab5338d9d8fba7111aa03b
Muzyria <fila090580@gmail.com>
2023-01-13 00:00:24 +0200

    new

--- place_5_10_1_HW.py
+++ place_5_10_1_HW.py
@@ -2,14 +2,14 @@ import requests
 
 
 class Test_new_location:
-    """Работа с новой локацией"""
+    """Работа с локацией"""
 
     def __init__(self):
         self.base_url = "https://rahulshettyacademy.com"  # базовая url
         self.key = "?key=qaclick123"  # Параметр для всех запросов
 
     def delete_new_location(self, place_id):
-        """Удаление новой локации методом DELETE"""
+        """Удаление локации методом DELETE"""
         delete_resource = "/maps/api/place/delete/json"
         delete_url = self.base_url + delete_resource + self.key
         print(delete_url)
@@ -38,16 +38,18 @@ class Test_new_location:
             assert 200 == result_get.status_code
             print(f"Успешно!!! Проверка локации №{num} для {place_id}")
             print()
-            return place_id
+            return True
         except Exception:
             print(f"Провал!!! Локация №{num} для {place_id} не найдена !!!")
             print()
+            return False
 
     def read_place_id_from_file(self):
         """Чтение place_id из файла и вызов метода GE, если локация есть то записывает place_id в list_place_id_2.txt"""
         with open("list_place_id.txt", "r", encoding='utf-8') as file, \
                 open("list_place_id_2.txt", "w", encoding='utf-8') as file_w:
-            [file_w.write(self.check_new_location(num, line)) for num, line in enumerate(file.read().split("\n"), 1) if line != '']
+            [file_w.write(line + "\n") if self.check_new_location(num, line) else ''
+             for num, line in enumerate(file.read().split("\n"), 1) if line != '']
 
     def read_place_id_and_delete(self):
         """Чтение place_id из файла и вызов метода DELETE"""
