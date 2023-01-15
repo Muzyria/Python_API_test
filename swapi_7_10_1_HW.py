84a4bc7fb87ca6dc03f92a837d58998c2d0795c7
Muzyria <fila090580@gmail.com>
2023-01-15 13:31:38 +0200

    new

--- swapi_7_10_1_HW.py
+++ swapi_7_10_1_HW.py
@@ -2,25 +2,30 @@ import requests
 
 
 class Swapi:
-    """Работа с """
+    """Работа с SW"""
 
     def __init__(self):
-        self.base_url = "https://swapi.dev"  # базовая url
-        # self.key = "?key=qaclick123"  # Параметр для всех запросов
+        self.base_url = 'https://swapi.dev/api'
 
-    def check_new(self):
-        get_resource = "/api/people/4/"
-        get_url = self.base_url + get_resource
+    def method_get(self, resource, key):
+        get_url = self.base_url + resource
         print(get_url)
         result_get = requests.get(get_url)
-        print(result_get.text)
+        return result_get.json()[key]
 
-    # def write_place_id_to_file(self):
-    #     """Создаем файл и записываем в него 5шт place_id"""
-    #     with open("list_place_id.txt", "w", encoding='utf-8') as file:
-    #         [file.write(self.create_new_location() + "\n") for _ in range(5)]
+    def write_all_characters(self):
+        """Создаем файл и записываем в него списрк персонажей"""
+        with open("list_characters.txt", "w", encoding='utf-8') as file:
+            all_people = set()
+            list_characters = []
+            all_films = [i.replace(self.base_url, "") for i in self.method_get('/people/4/', 'films')]
+            print(all_films)
+            for item in all_films:
+                [all_people.add(i.replace(self.base_url, "")) for i in self.method_get(item, "characters")]
+            for item in all_people:
+                list_characters.append(self.method_get(item, 'name'))
+            [file.write(line + "\n") for line in sorted(list_characters)]
 
 
-new_place = Swapi()
-new_place.check_new()
-
+dw_check = Swapi()
+dw_check.write_all_characters()
